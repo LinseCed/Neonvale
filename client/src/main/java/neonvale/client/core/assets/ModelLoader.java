704eaff9764d1609cde8bcb799ee7e197d38ca3b
Linus Bauer <bauerlinus11@gmail.com>
2026-01-25 22:30:03 +0100

    Quick Add

--- client/src/main/java/neonvale/client/core/assets/ModelLoader.java
+++ client/src/main/java/neonvale/client/core/assets/ModelLoader.java
@@ -1,5 +1,6 @@
 package neonvale.client.core.assets;
 
+import org.joml.Vector4f;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.assimp.*;
 
@@ -38,12 +39,28 @@ public class ModelLoader {
             Material material = new Material();
             AIMaterial mat = AIMaterial.create(scene.mMaterials().get(i));
 
+            // Material Name
             AIString name = AIString.calloc();
             Assimp.aiGetMaterialString(mat, Assimp.AI_MATKEY_NAME, aiTextureType_NONE, 0, name);
             material.name = name.dataString();
 
+            // Base Color Factor
             AIColor4D color = AIColor4D.create();
             int result = Assimp.aiGetMaterialColor(mat, AI_MATKEY_BASE_COLOR, aiTextureType_NONE, 0, color);
+            if (result == aiReturn_SUCCESS) {
+                material.baseColorFactor = new Vector4f(color.r(), color.g(), color.b(), color.a());
+            }
+
+            AITexture albedoTex;
+            AIString albedoTexpath = AIString.calloc();
+            result = Assimp.aiGetMaterialTexture(mat, aiTextureType_BASE_COLOR, 0, albedoTexpath, (IntBuffer) null, (IntBuffer) null, (FloatBuffer) null, (IntBuffer) null, (IntBuffer) null, (IntBuffer) null);
+            if (result == aiReturn_SUCCESS) {
+                material.albedoTex = new Texture(albedoTexpath.data()).getId();
+            }
+
+            float[] tmp = new float[1];
+
+            result = Assimp.aiGetMaterialFloatArray(mat, AI_MATKEY_METALLIC_FACTOR, aiTextureType_NONE, 0, tmp,null);
         }
     }
 }
